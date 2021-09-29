f8e60ca162528ab68b7c163d377ec89b644f68e4
brenno-silva <brenno.silva037@gmail.com>
2021-09-29 11:26:35 -0300

    #7 - Alteração na model da receita

--- receita/models.py
+++ receita/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 DIFICULDADES_ESCOLHAS = [
-	('F','Fácil'),
-	('M','Médio'),
-	('D','Difícil'),
+    ('I', 'Iniciante'),
+    ('T', 'Intermediario'),
+    ('A', 'Avancado'),
 ]
+
 
 class Categoria(models.Model):
     nome_categoria = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome_categoria
+
 
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=50)
@@ -19,6 +21,6 @@ class Receita(models.Model):
     categoria_receita = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     modo_preparo = models.TextField()
     dificuldade = models.CharField(max_length=1, choices=DIFICULDADES_ESCOLHAS)
-    
+
     def __str__(self):
         return self.nome_receita
