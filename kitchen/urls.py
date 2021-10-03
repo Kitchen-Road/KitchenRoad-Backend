36ebb78ba0f843844b086d9988934a0007312944
brenno-silva <brenno.silva037@gmail.com>
2021-10-03 12:39:31 -0300

    Adição de url para imagens

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from django.urls.conf import include
 from rest_framework import routers
 from receita import views as categoriaviewsets
-from dica import views as dicasviewsets
 route = routers.DefaultRouter()
 
 route.register(r'categorias', categoriaviewsets.CategoriaViewSet)
@@ -29,4 +30,4 @@ urlpatterns = [
     path('receitas/', include('receita.urls', 'receita')),
     path('cozinheiro/', include('cozinheiro.urls', 'cozinheiro')),
     path('', include(route.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
