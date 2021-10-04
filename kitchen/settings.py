562b78d640cc8f4b91ce5ae7b3a5bf60819811ee
brenno-silva <brenno.silva037@gmail.com>
2021-10-04 19:54:18 -0300

    Alteração no tempo de validade do token

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
+from rest_framework.settings import api_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,3 +145,8 @@ REST_FRAMEWORK = {
 # CORS configuration
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+# Knox configuration
+REST_KNOX = {
+    'TOKEN_TTL': timedelta(hours=200),
+}
