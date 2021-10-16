7e8b6fd95920b648397822fe075fb45769250499
brenno-silva <brenno.silva037@gmail.com>
2021-10-16 13:28:16 -0300

    Adição da funcionalidade de concluir receita

--- cozinheiro/urls.py
+++ cozinheiro/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.urls.conf import include
 from rest_framework import routers
-from .views import RegisterCozinheiroView, LoginCozinheiroView
+from .views import RegisterCozinheiroView, LoginCozinheiroView, ReceitaCompletadaView
 
 app_name = "cozinheiro"
 
 urlpatterns = [
     path('signup/', RegisterCozinheiroView.as_view(), name="signup"),
     path('login/', LoginCozinheiroView.as_view(), name="login"),
+    path('concluir-receita/', ReceitaCompletadaView.as_view(),
+         name="concluir_receita"),
 ]
